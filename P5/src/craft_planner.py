92aa2e6bd9312435ebddbc08bf651c5f93a63aa5
jboy5544123 <jocobrow@ucsc.edu>
2020-11-06 16:26:04 -0800

    tried to go backwards without a* current problem is that 'produces' cannot be assigned within reverse_effect need to change it to forward a* using recipes

--- P5/src/craft_planner.py
+++ P5/src/craft_planner.py
@@ -61,14 +61,15 @@ def make_effector(rule):
         # Tip: Do something with rule['Produces'] and rule['Consumes'].
         next_state = None
         next_state = state
-        for items_to_delete, num_items_to_delete in rule['Consumes'].items():
-            if(items_to_delete in next_state):
-                if(next_state[items_to_delete] > num_items_to_delete):
-                    next_state[items_to_delete] = next_state[items_to_delete]-num_items_to_delete
+        if 'Consumes' in rule:
+            for items_to_delete, num_items_to_delete in rule['Consumes'].items():
+                if(items_to_delete in next_state):
+                    if(next_state[items_to_delete] > num_items_to_delete):
+                        next_state[items_to_delete] = next_state[items_to_delete]-num_items_to_delete
+                    else:
+                        next_state.pop(items_to_delete)
                 else:
-                    next_state.pop(items_to_delete)
-            else:
-                return False
+                    return False
 
         for items_to_add, num_items_to_add in rule['Produces'].items():
             if(items_to_add in next_state):
@@ -79,6 +80,37 @@ def make_effector(rule):
         return next_state
 
     return effect
+
+#this is useless since 'Produces' cant be assigned to anything
+def reverse_effect(state, goal):
+    print("running reverse_effect on: ")
+    print(state)
+    next_state = None
+    next_state = state
+    print("Produces is ")
+    print(all_recipes(rule['Produces'])
+    for items_to_delete, num_items_to_delete in rule['Produces'].items():
+        if(items_to_delete in next_state):
+            if(next_state[items_to_delete] > num_items_to_delete):
+                next_state[items_to_delete] = next_state[items_to_delete]-num_items_to_delete
+            else:
+                next_state.pop(items_to_delete)
+        else:
+            print(items_to_delete)
+            print(" is not in ")
+            print(next_state)
+            return False
+
+    for items_to_add, num_items_to_add in rule['Consumes'].items():
+        if(items_to_add in next_state):
+            next_state[items_to_add] = next_state[items_to_add]+num_items_to_add
+        else:
+            next_state[items_to_add] = num_items_to_add
+
+    print("next state is: ")
+    print(state)
+    return next_state
+
 
 
 def make_goal_checker(goal):
@@ -93,7 +125,7 @@ def make_goal_checker(goal):
             else:
                 if(state[items_needed] < num_items_needed):
                     return False
-                    
+
         return True
 
     return is_goal
@@ -107,12 +139,26 @@ def graph(state):
         if r.check(state):
             yield (r.name, r.effect(state), r.cost)
 
+def costly_graph(state):
+    # Iterates through all recipes/rules, checking which are valid in the given state.
+    # If a rule is valid, it returns the rule's name, the resulting state after application
+    # to the given state, and the cost for the rule.
+    #print ('running costly_graph on: ')
+    #print (state)
+    #print ("")
+    list = []
+    for r in all_recipes:
+        if r.check(state):
+            list.append((r.name, r.effect(state), r.cost))
+    #print("all possible things to craft from current state: ")
+    #print(list)
+    return list
 
 def heuristic(state):
     # Implement your heuristic here!
     return 0
 
-def search(graph, state, is_goal, limit, heuristic):
+def search(graph, state, is_goal, limit, heuristic, Goal):
 
     start_time = time()
 
@@ -120,9 +166,51 @@ def search(graph, state, is_goal, limit, heuristic):
     # When you find a path to the goal return a list of tuples [(state, action)]
     # representing the path. Each element (tuple) of the list represents a state
     # in the path and the action that took you to this state
-    while time() - start_time < limit:
-        next(graph(state))
-        pass
+    path = []
+    reverse_path = []
+    current_state = Goal
+
+    #while time() - start_time < limit:
+    while True:
+        #next(graph(state))
+
+
+        for current_items, num_current_items in current_state.items():
+            if(current_items in state):
+                if(num_current_items > state[current_items]):
+                    current_state[current_items] = current_state[current_items] - state[current_items]
+                elif(num_current_items == state[current_items]):
+                    current_state.pop(current_items)
+
+        if(current_state == {}):
+            break
+
+        previous_state = current_state
+        current_state = reverse_effect(current_state)
+
+        #item = next(graph(state))
+        for next_item in costly_graph(state):
+            if(next_item[1] == previous_state):
+                reverse_path.append(current_state, next_item[0])
+                break
+
+
+
+
+
+        #next = (graph(state))
+        #path.append(state, next[0])
+        #state = next[1]
+
+    #reversing the path
+    print(reverse_path)
+    while(len(reverse_path) > 0):
+        path.append(reverse_path[-1])
+        reverse_path.pop()
+
+    print(path)
+    return path
+    pass
 
     # Failed to find a path
     print(time() - start_time, 'seconds.')
@@ -161,7 +249,7 @@ if __name__ == '__main__':
     state.update(Crafting['Initial'])
 
     # Search for a solution
-    resulting_plan = search(graph, state, is_goal, 5, heuristic)
+    resulting_plan = search(graph, state, is_goal, 5, heuristic, Crafting['Goal'])
 
     if resulting_plan:
         # Print resulting plan
